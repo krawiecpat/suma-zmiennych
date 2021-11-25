121ba7ab53d4e4c3c056602b1f30bd521c336982
krawiecpat <krawiecpat9@gmail.com>
2021-11-25 19:52:22 +0100

    suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let firstNumber = 11;
+
+secondNumber = 66;
+
+function multiply(firstNumber, secondNumber) {
+    let result = firstNumber * secondNumber;
+    let suma = 726;
+    return result, suma;
+}
+
+console.log(multiply(66, 11));
+
+let wynikMnozenia = multiply(firstNumber, secondNumber);
+
+console.log(wynikMnozenia);
